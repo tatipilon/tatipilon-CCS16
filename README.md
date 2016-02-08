80397dfc32eda179234b83ba7f3bbe885d5e6d8a
Tatiana Pilon <tatianapilon@Tatianas-MBP.fios-router.home>
2016-02-07 22:11:40 -0500

    Second week homework

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# tatipilon-CCS16
-# tatipilon-CCS16
-# tatipilon-CCS16
+Tatiana Pilon
+
+Hello!
+
+My name is Tatiana Pilon and this is my first repository using P5.js.  
+
+
---
+++ Still Remembered draft P5.js
@@ -0,0 +1,17 @@
+Still Remembered draft P5.js
+
+function setup() {
+  createCanvas(1000,1000);
+  background ('rgba(51,112,115,0.25)');
+}
+
+function draw() {
+  background ('rgba(51,112,115,0.25)');
+  rect(100,100,850,650);
+  fill ('rgba(223,185,56,1)');
+  rect(200,250,650,455);
+  fill ('rgba(127,87,17,1)');
+  rect(400,350,300,300)
+  fill('rgba(252,224,54,1)');
+  
+}
